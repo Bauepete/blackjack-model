aaa192cc1a8e6ffcaf3a3b28e1db031b364507d7
Peter Bauer <p.bauer@htl-leonding.ac.at>
2023-02-08 14:31:44 +0100

    Simple value of hand works

--- Sources/BlackJackModel/Hand.swift
+++ Sources/BlackJackModel/Hand.swift
@@ -5,24 +5,10 @@
 //  Created by Peter Bauer on 02.02.23.
 //
 
-struct Hand: Collection {
-    var startIndex: Int = 0
-    var endIndex: Int = 0
-    var count: Int {
-        return cardsInHand.count
-    }
-    
-    private var cardsInHand: [Card] = []
-    
-    subscript(position: Int) -> Card {
-        return cardsInHand[position]
-    }
-    
-    func index(after i: Int) -> Int {
-        return 0
-    }
-    
-    mutating func append(_ newElement: Card) {
-        cardsInHand.append(newElement)
+typealias Hand = [Card]
+
+extension Array where Element == Card {
+    var value: Int {
+        self.reduce(0, { x, y in x + y.value })
     }
 }
--- Tests/BlackJackModelTests/HandTests.swift
+++ Tests/BlackJackModelTests/HandTests.swift
@@ -44,10 +44,41 @@ final class HandTests: XCTestCase {
         
         hand.append(firstCard)
         hand.append(secondCard)
-        
         let firstCardFromHand = hand[0]
         let secondCardFromHand = hand[1]
         
         XCTAssertEqual(firstCard, firstCardFromHand)
+        XCTAssertEqual(secondCard, secondCardFromHand)
+    }
+    
+    func testThatItShouldReturnAnArrayOfCards_GivenASlice() {
+        let firstCard = Card(rank: .two, suit: .clubs)
+        let secondCard = Card(rank: .ace, suit: .spades)
+        
+        hand.append(firstCard)
+        hand.append(secondCard)
+        hand.append(firstCard)
+        hand.append(secondCard)
+        hand.append(firstCard)
+        let returnedCards = hand[1..<3]
+        
+        XCTAssertEqual(2, returnedCards.count)
+        XCTAssertEqual(secondCard, returnedCards[1])
+        XCTAssertEqual(firstCard, returnedCards[2])
+    }
+    
+    func testThatValueIs11_GivenOneAce() {
+        hand.append(Card(rank: .ace, suit: .clubs))
+        
+        XCTAssertEqual(11, hand.value)
+    }
+    
+    func testThatItsValueIs13_GivenAnAceAndATwo() {
+        let someAce = Card(rank: .ace, suit: .clubs)
+        let someTwo = Card(rank: .two, suit: .diamonds)
+        hand.append(someAce)
+        hand.append(someTwo)
+        
+        XCTAssertEqual(13, hand.value)
     }
 }
