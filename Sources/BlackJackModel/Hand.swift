b5a189d904ea074eb1a4826ae6794045ee81abeb
Peter Bauer <p.bauer@htl-leonding.ac.at>
2023-02-09 15:30:41 +0100

    Refactored extraction of aces

--- Sources/BlackJackModel/Hand.swift
+++ Sources/BlackJackModel/Hand.swift
@@ -9,9 +9,13 @@ typealias Hand = [Card]
 
 extension Array where Element == Card {
     var value: Int {
-        let normalCardsValue = self.reduce(0, { $0 + ($1.rank == Card.Rank.ace ? 0 : $1.value) })
-        let acesCount = self.reduce(0, { $0 + ($1.rank == Card.Rank.ace ? 1 : 0) })
-        let acesArray = Array(repeating: Card(rank: .ace, suit: .clubs), count: acesCount)
-        return acesArray.reduce(normalCardsValue, { $0 + ($0 >= 11 ? 1 : $1.value) })
+        let valueOfCardsBeingNoAce = self.reduce(0, { $0 + ($1.rank == Card.Rank.ace ? 0 : $1.value) })
+        let acesArray = self.filter({ $0.rank == .ace})
+        let thresholdFromWhichAcesToBeCountedAsOne = 11
+        return acesArray.reduce(valueOfCardsBeingNoAce,
+                                {
+                                    $0 + ($0 >= thresholdFromWhichAcesToBeCountedAsOne ? 1 : $1.value)
+                                    
+                                })
     }
 }
