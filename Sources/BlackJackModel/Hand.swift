8ea7068a9ea1bd12a33fab1e6157ae0475fd7706
Peter Bauer <p.bauer@htl-leonding.ac.at>
2023-02-12 21:04:36 +0100

    Refactored if to ternary operator

--- Sources/BlackJackModel/Hand.swift
+++ Sources/BlackJackModel/Hand.swift
@@ -10,14 +10,11 @@ typealias Hand = [Card]
 extension Array where Element == Card {
     var value: Int {
         let valueWithAcesCountingOne = self.reduce(0, { $0 + ($1.rank == Card.Rank.ace ? 1 : $1.value) })
-        if valueWithAcesCountingOne < 12 && handContainsAnAce()  {
-            return valueWithAcesCountingOne + 10
-        } else {
-            return valueWithAcesCountingOne
-        }
+        let oneAceMustCountEleven: Bool = valueWithAcesCountingOne < 12 && handContainsAnAce()
+        return oneAceMustCountEleven ? valueWithAcesCountingOne + 10 : valueWithAcesCountingOne
     }
     
     fileprivate func handContainsAnAce() -> Bool {
-        return self.contains(where: { $0.rank == .ace })
+        return self.contains { $0.rank == .ace }
     }
 }
